8e86b092b663ebd4f5e08c72cfb80215bb8c1bef
Eti Calde <etienne.rojas@usm.cl>
2023-04-11 19:13:29 -0400

    [MP-5] message exchange

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 from threading import Thread
 
 # server's IP address
-SERVER_HOST = "0.0.0.0"
+SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5002 # port we want to use
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
@@ -18,18 +18,41 @@ s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
 print(f"[*] Servidor escuchando en {SERVER_HOST}:{SERVER_PORT}")
 
-def new_client():
-    print(f"[+] Nuevo Cliente!")
+def listen_for_client(cs, socket_number):
+    while True:
+        try:
+            # keep listening for a message from `cs` socket
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            # client no longer connected
+            # remove it from the set
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            # if we received a message, replace the <SEP> 
+            # token with ": " for nice printing
+            if(msg == "q"):
+                print("Un cliente se ha desconectado")
+                cs.close()
+                client_sockets.remove(cs)
+                break
+
+            msg = msg.replace(separator_token, ": ")
+            print(f"mensaje recibido: {msg}")
+        # iterate over all connected sockets
+        for client_socket in client_sockets:
+            # and send the message
+            client_socket.send(msg.encode())
 
 
 while True:
     # we keep listening for new connections all the time
     client_socket, client_address = s.accept()
-    print(f"[*] Dirección del cliente {client_address}")
+    print(f"[+] Nuevo Cliente: {client_address}.")
     # add the new connected client to connected sockets
     client_sockets.add(client_socket)
-    # start a new thread 
-    t = Thread(target=new_client)
+    # start a new thread that listens for each client's messages
+    t = Thread(target=listen_for_client, args=(client_socket,client_socket))
     # make the thread daemon so it ends whenever the main thread ends
     t.daemon = True
     # start the thread
