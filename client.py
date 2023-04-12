c9fd525f987c07704c714ee4c4df506e68235c96
Eti Calde <etienne.rojas@usm.cl>
2023-04-11 20:10:11 -0400

    [MP-7] communication is protected

--- client.py
+++ client.py
@@ -1,8 +1,27 @@
 import socket
 import random
+import re
 from threading import Thread
 from datetime import datetime
 from colorama import Fore, init, Back
+def encryptAuthoring(message):
+    ret = []
+    for character in message:
+        ret.append( str((ord(character)+14)) )
+    ret.reverse()
+    return (','.join(ret))
+
+
+def decodeAuthoring(message):
+    ret = message.split(',')
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    ret[-1] = ansi_escape.sub('', ret[-1])    
+    ret.reverse()
+    for character in ret:
+        actualCharac = character
+        newCharac = (chr(int(character)-14))
+        ret[ret.index(actualCharac)] = newCharac
+    return (''.join(ret))
 
 # init colors
 init()
@@ -36,7 +55,9 @@ name = input("Ingrese su nombre: ")
 def listen_for_messages():
     while True:
         message = s.recv(1024).decode()
-        print("\n" + message)
+        importantMessage = message.split(": ")
+        decodeMsg = decodeAuthoring(importantMessage[1])
+        print("\n" + importantMessage[0], decodeMsg)
 
 # make a thread that listens for messages to this client & print them
 t = Thread(target=listen_for_messages)
@@ -47,14 +68,14 @@ t.start()
 
 while True:
     # input message we want to send to the server
-    to_send =  input()
+    to_send =  input().rstrip()
     # a way to exit the program
     if to_send.lower() == 'q':
         s.send(to_send.encode())
         break
     # add the datetime, name & the color of the sender
     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{encryptAuthoring(to_send)}{Fore.RESET}"
     # finally, send the message
     s.send(to_send.encode())
 s.close()
